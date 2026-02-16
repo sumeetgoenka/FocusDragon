46ab500b74ad6871385768dc7f1bafd11f2957d5
Anay Goenka <anaygoenka12@gmail.com>
2026-02-16 14:59:39 +0400

    Initial Commit

---
+++ FocusDragon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusDragon
+//
+//  Created by Anay Goenka on 16/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusDragon/FocusDragonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusDragonApp.swift
+//  FocusDragon
+//
+//  Created by Anay Goenka on 16/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct FocusDragonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
